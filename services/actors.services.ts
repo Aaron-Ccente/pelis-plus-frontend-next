38f7d0bd29646e4d4ddfc0495e0397a87e489880
Aaron-Ccente <aronccente@gmail.com>
2026-02-16 21:57:55 -0500

    feat: api para actors

---
+++ services/actors.services.ts
@@ -0,0 +1,11 @@
+import { Actor } from "@/types/Actors";
+import { apiFetch } from "./api";
+import { PaginatedResponse } from "@/types/Pagination";
+
+export async function getMovies(
+  page: number = 1
+): Promise<PaginatedResponse<Actor>> {
+  return apiFetch<PaginatedResponse<Actor>>(
+    `/actors?page=${page}`
+  );
+}
---
+++ types/Actors.ts
@@ -0,0 +1,6 @@
+export type Actor =  {
+    "name": string,
+    'image_actor': string,
+    'biography': string,
+    'date_of_birth': Date
+}
